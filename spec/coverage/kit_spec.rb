41f82d48d568d94ccc85e9601850a30bbb03b4d9
John Mortlock <john.mortlock@gmail.com>
2021-01-06 11:41:48 +1030

    Update to build with github actions / ruby 3.0 / rails 6.1 (#1)
    Co-authored-by: John Mortlock <john.mortlock@sealink.com.au>

--- spec/coverage/kit_spec.rb
+++ spec/coverage/kit_spec.rb
@@ -4,8 +4,4 @@ describe Coverage::Kit do
   it "has a version number" do
     expect(Coverage::Kit::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
